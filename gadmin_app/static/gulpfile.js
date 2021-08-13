c51613e5f9e33819b5d3c3587bad2155f296a1e6
genadyd <g.dorodny@duplitrade.com>
2021-08-13 16:17:43 +0300

    add bootstrap and include it into main scss file

--- gadmin_app/static/gulpfile.js
+++ gadmin_app/static/gulpfile.js
@@ -1,11 +1,11 @@
 const {src, dest, watch} = require('gulp')
 const scss = require('gulp-sass')(require('sass'))
-// scss.compiller = require('node-sass')
 
 
 function compile_scss() {
   return src(['./styles/assets/**/*.scss'])
     .pipe(scss().on('error', scss.logError))
+    .pipe(scss({outputStyle: 'compressed'}))
     .pipe(dest('./styles/dest/'))
 }
 
