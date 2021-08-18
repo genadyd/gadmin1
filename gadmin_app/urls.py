e1c1eb5f0b7243e1b72d80481323b002881886b9
genadyd <g.dorodny@duplitrade.com>
2021-08-18 13:02:04 +0300

    title and searchform add

--- gadmin_app/urls.py
+++ gadmin_app/urls.py
@@ -3,10 +3,11 @@ from .views.CategoriesViews import CategoriesViews
 from .views.ContentViews import ContentViews
 from .views.DashboardViews import DashboardViews
 
-pattern = '[\w]*$|^[\w]*$'
+dashboard_pattern = '[\w]*$|^[\w]*$'
+pattern = '[\w]*$'
 
 urlpatterns = [
     re_path(r'categories/' + pattern, CategoriesViews.as_view()),
     re_path(r'content/' + pattern, ContentViews.as_view()),
-    re_path(r'^dashboard/' + pattern, DashboardViews.as_view()),
+    re_path(r'^dashboard/' + dashboard_pattern, DashboardViews.as_view()),
 ]
