6c587b5b53f2057deccd46aaeaa3488057bc2792
genadyd <g.dorodny@duplitrade.com>
2021-08-30 11:24:12 +0300

    correct the MainMenu migration

--- gadmin_app/admin.py
+++ gadmin_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from gadmin_app.models import MainMenu, MainMenuCategories
+from gadmin_app.models import MainMenu, MainMenuCategory
 
-admin.site.register(MainMenuCategories)
+admin.site.register(MainMenuCategory)
 admin.site.register(MainMenu)
--- gadmin_app/models.py
+++ gadmin_app/models.py
@@ -6,27 +6,26 @@ class MainMenu(models.Model):
     title = models.CharField(max_length=255, verbose_name="Menu Title")
     description = models.TextField(verbose_name="Description")
     admin_description = models.TextField(verbose_name="Admin Description")
-    url = models.URLField(verbose_name="URL")
-    parent = models.ForeignKey('gadmin_app.MainMenuCategories', to_field='id', verbose_name="Parent", null=True,
+    url = models.CharField(verbose_name="URL", max_length=255)
+    parent = models.ForeignKey('gadmin_app.MainMenuCategory', to_field='id', verbose_name="Parent", null=True,
                                blank=True,
                                on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"id: {self.id}title: {self.title}, description: {self.description}"
+        return f"id: {self.id}title: {self.title}, description: {self.admin_description}"
 
     class Meta:
         db_table = 'main_menu'
 
 
-class MainMenuCategories(models.Model):
+class MainMenuCategory(models.Model):
     title = models.CharField(max_length=255, verbose_name="Menu Category Title")
     description = models.TextField(verbose_name="Description")
     admin_description = models.TextField(verbose_name="Admin Description")
 
     def __str__(self):
         return f"""id: {self.id}
-                title: {self.title}
-                description: {self.description}"""
+                title: {self.title}"""
 
     class Meta:
         db_table = 'main_menu_categories'
